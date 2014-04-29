fa472bfaad84e991ef70d54923533afebe954e0a
tim hsu <tim.yellow@gmail.com>
2014-04-29 21:29:15 +0800

    add basic support

--- HerokuSample.py
+++ HerokuSample.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
